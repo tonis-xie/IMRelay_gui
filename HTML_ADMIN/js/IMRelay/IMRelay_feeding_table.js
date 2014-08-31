ca140e6c541fa866dbb2df576cdfc5cd964a68fa
Joakim Myrland <JoakimMyrland@hotmail.com>
2014-08-31 23:32:09 +0200

    added dynatable to feeding table, and slimmed down dependancies

--- HTML_ADMIN/js/IMRelay/IMRelay_feeding_table.js
+++ HTML_ADMIN/js/IMRelay/IMRelay_feeding_table.js
@@ -1,12 +1,98 @@
 $(function () {
 
-    $('#toogle_edit_button').click( function() {
+    var table = new vis.DataSet();
 
-        console.log('button clicked');
+    for (var i = 1; i <= 16; i++) {
+        table.add({
+            id: i,
+            state: 'inactive',
+            start_date: new Date().setHours(0, 0, 0, 0),
+            nr_of_fish: '0',
+            avg_fish_kg: '1',
+            biomass: '1',
+            feeder_speed_kg_pr_min: '1',
+            feeding_percent: '1',
+            required_feed_pr_day: '1',
+            feed_progress_today: '1',
+            time_feeding_intervals: '1',
+            time_feeder_active: '1',
+            feeder_toggle_speed: '1'
+        });
+    }
+
+    var view = new vis.DataView(table, {
+        fields: ['id', 'state', 'start_date', 'nr_of_fish', 'avg_fish_kg',
+                     'feeder_speed_kg_pr_min', 'feeding_percent']
+    });
+
+    view.on('*', function (event, properties, senderId) {
+
+        var feeder_table_to_localstorage = view.get();
+        localStorage["feeder.table"] = JSON.stringify(feeder_table_to_localstorage);
+
+    });
+    
+    var myRecords = table.get();
+    table.on('*', function (event, properties, senderId) {
+
+        myRecords = table.get();
+
+    });
+
+    table.update([
+        { id: 1, state: 'active' },
+        { id: 2, state: 'active' }
+    ]);
+
+    function feeding_table_row_writer(rowIndex, record, columns, cellWriter) {
+
+        var tr = '';
+
+        var start_date_datetime_object = new Date(record.start_date);
+        var start_date_datetime_string =        start_date_datetime_object.getFullYear().toString() + '-' +
+                                        ("0" + (start_date_datetime_object.getMonth() + 1).toString()).slice(-2) + '-' +
+                                        ("0" +  start_date_datetime_object.getDate().toString()).slice(-2);
+        
+        record.start_date = start_date_datetime_string;
+        record.biomass = record.nr_of_fish * record.avg_fish_kg;
+        record.required_feed_pr_day = record.biomass * record.feeding_percent;
+        record.time_feeder_active = record.required_feed_pr_day / record.feeder_speed_kg_pr_min;
+        record.feeder_toggle_speed = record.time_feeder_active / record.time_feeding_intervals;
+
+        // grab the record's attribute for each column
+        for (var i = 0, len = columns.length; i < len; i++) {
+            tr += cellWriter(columns[i], record);
+        }
+
+        return '<tr>' + tr + '</tr>';
+    }
+    
+    $('#feeding_table').dynatable({
+        features: {
+            paginate: false,
+            sorting: false,
+            recordCount: false,
+            search: false
+        },
+        dataset: {
+            records: myRecords
+        },
+        writers: {
+            _rowWriter: feeding_table_row_writer
+        }
+    });
+    
+    var dynatable = $('#feeding_table').data('dynatable');
+    dynatable.sorts.clear();
+    dynatable.sorts.add('id', 1); // 1=ASCENDING, -1=DESCENDING
+    dynatable.process();
+
+    $('#toogle_edit_button').click(function () {
 
         var cell = $('table#feeding_table tr td');
         var is_editable = cell.is('.editable');
-        cell.prop('contenteditable', !is_editable).toggleClass('editable')
-    })
+        cell.prop('contenteditable', !is_editable).toggleClass('editable');
+
+    });
 
 });
--- HTML_ADMIN/js/IMRelay/IMRelay_vis.js
+++ HTML_ADMIN/js/IMRelay/IMRelay_vis.js
@@ -20,11 +20,12 @@ $(function () {
    
     groups.on('*', function (event, properties, senderId) {
 
-        var temp_12345 = groups.get({
-            fields: ['id', 'className']        // output the specified fields only
+        var group_classnames_to_localstorage = groups.get({
+            // output the specified fields only
+            fields: ['id', 'className']
         });
 
-        localStorage["timeline.groups"] = JSON.stringify(temp_12345);
+        localStorage["timeline.groups"] = JSON.stringify(group_classnames_to_localstorage);
 
     });
 
@@ -33,7 +34,7 @@ $(function () {
     var end_date = new Date();
     end_date.setTime(start_date.getTime() + (24 * 60 * 60 * 1000));
 
-    function parse_timeline_from_localstorage(data) {
+    function parse_timeline_items_json(data) {
 
         for (key in data) {
 
@@ -45,13 +46,13 @@ $(function () {
     }
 
     var items = new vis.DataSet();
-    var local_storage_timeline_items = localStorage["timeline.items"];
+    var timeline_items_localstorage = localStorage["timeline.items"];
 
-    if (local_storage_timeline_items != null) {
+    if (timeline_items_localstorage != null) {
 
-        var string_dataset = JSON.parse(local_storage_timeline_items);
-        parse_timeline_from_localstorage(string_dataset);
-        items["_data"] = string_dataset;
+        var timeline_items_json = JSON.parse(timeline_items_localstorage);
+        parse_timeline_items_json(timeline_items_json);
+        items["_data"] = timeline_items_json;
     }
 
     items.on('*', function (event, properties, senderId) {
@@ -136,23 +137,15 @@ $(function () {
 
     function enable_vis_timeline_button_classes(btn_num) {
 
-        console.log(btn_num + 'enabled func');
-
-        //$("#vis_group_button_" + btn_num).parent().parent().removeClass('vis_group_disabled').addClass('vis_group_enabled');
         $("#vis_group_button_" + btn_num).removeClass('btn-default vis_btn_disabled').addClass('btn-success vis_btn_enabled');
         $("#vis_group_button_" + btn_num).html(btn_num + ': Enabled');
-        //$(".vis.timeline .foreground > div:nth-child(" + btn_num + ")").removeClass('vis_group_disabled').addClass('vis_group_enabled');
 
     }
 
     function disable_vis_timeline_button_classes(btn_num) {
 
-        console.log(btn_num + 'disabled func');
-
-        //$("#vis_group_button_" + btn_num).parent().parent().removeClass('vis_group_enabled').addClass('vis_group_disabled');
         $("#vis_group_button_" + btn_num).removeClass('btn-success vis_btn_enabled').addClass('btn-default vis_btn_disabled');
         $("#vis_group_button_" + btn_num).html(btn_num + ': Disabled');
-        //$(".vis.timeline .foreground > div:nth-child(" + btn_num + ")").removeClass('vis_group_enabled').addClass('vis_group_disabled');
 
     }    
 
@@ -176,22 +169,18 @@ $(function () {
         toggle_vis_timeline_button_classes(btn_num);
     }
 
-    var tem_5513 = localStorage["timeline.groups"];
+    var timeline_groups_localstorage = localStorage["timeline.groups"];
 
-    if (tem_5513 != null) {
-        var string_dataset2 = JSON.parse(tem_5513);
-        groups.update(string_dataset2);
+    if (timeline_groups_localstorage != null) {
+        var timeline_groups_json = JSON.parse(timeline_groups_localstorage);
+        groups.update(timeline_groups_json);
 
-        for (var cntr_var_temp = 0; cntr_var_temp < 16; cntr_var_temp++) {
+        for (var group_index = 0; group_index < 16; group_index++) {
 
-
-            console.log(string_dataset2[cntr_var_temp].className);
-            if (string_dataset2[cntr_var_temp].className == 'vis_group_disabled') {
-                disable_vis_timeline_button_classes(cntr_var_temp + 1);
-                console.log(cntr_var_temp + 'disabled');
+            if (timeline_groups_json[group_index].className == 'vis_group_disabled') {
+                disable_vis_timeline_button_classes(group_index + 1);
             } else {
-                enable_vis_timeline_button_classes(cntr_var_temp + 1);
-                console.log(cntr_var_temp + 'enabled');
+                enable_vis_timeline_button_classes(group_index + 1);
             }
 
         }
